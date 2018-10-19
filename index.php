19d65e9f6a78aa7be2c966cee9f5d45099c823ea
ganeshpd188 <37778502+ganeshpd188@users.noreply.github.com>
2018-10-20 01:23:06 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang=”en”> 
+<head>
+ <meta charset="UTF-8">
+	<title>Resume Builder</title>
+</head>
+<body>
+<a href="index2.php">Those who have Percentage system in college,Please go through this link</a><br/><br/>
+<a href="index1.php">Those who have CGPA system in college,Please go through this link</a><br/><br/>
+</body>
+</html>
